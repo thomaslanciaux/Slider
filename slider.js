d580cfc97228f04110e433f602c2ebe2080d9d0b
thomaslanciaux <thomaslanciaux4@gmail.com>
2011-12-20 11:19:03 +0000

    JS min fix

--- slider.js
+++ slider.js
@@ -3,7 +3,7 @@
 	// ===========================
 	
 	Slider = function(el, options){
-		var obj = this
+		var obj = this;
 		
 		// Default options
 		obj.options = {
@@ -24,53 +24,53 @@
 			current = 1,
 			direction = 'ltr', 
 			autoTimer = null,
-			animated = false
+			animated = false;
 		
 		// Count steps
-		slider.steps = function(){ return slider.children().length }
+		slider.steps = function(){ return slider.children().length; }
 		
 		// Get elts dimensions
 		slider.getDim = function(){
-			var slide = slider.children()
-			dims.slideW = slide.dim().width
-			dims.slideH = slide.dim().height
-			dims.wrapW = dims.slideW*(slider.steps())
-			return dims
+			var slide = slider.children();
+			dims.slideW = slide.dim().width;
+			dims.slideH = slide.dim().height;
+			dims.wrapW = dims.slideW*(slider.steps());
+			return dims;
 		}
 		
 		// Set elts dimensions
 		slider.draw = function(){
-			slider.getDim()
-			wrapper.css({ width: dims.slideW })
+			slider.getDim();
+			wrapper.css({ width: dims.slideW });
 			slider.css({ 
 				left: '-'+dims.slideW*(current-1), 
-				width: dims.wrapW, height: dims.slideH 
-			})
+				width: dims.wrapW, height: dims.slideH
+			});
 		}
 		
 		slider.paginate = function(){
-			var pageC = $('<p class="pagination"></p>').appendTo($(el))
+			var pageC = $('<p class="pagination"></p>').appendTo($(el));
 				
 			for ( var i=0; i<slider.steps(); i++ ) {
-				pageLinks[i] = $('<input type="button" class="slide" data-slide="'+(i+1)+'" value="'+(i+1)+'">')
-				pageLinks[i].appendTo(pageC)
+				pageLinks[i] = $('<input type="button" class="slide" data-slide="'+(i+1)+'" value="'+(i+1)+'">');
+				pageLinks[i].appendTo(pageC);
 			}
 			
 			$(pageLinks).each(function(){
-				var link = $(this)
+				var link = $(this);
 				link.bind({
 					click : function(){
 						if ( !animated ) {
-							slider.slideTo(parseFloat(link.attr('data-slide'))) 
-							link.trigger('setActive')
+							slider.slideTo(parseFloat(link.attr('data-slide')));
+							link.trigger('setActive');
 						}
 					},
 					setActive : function(){
 						$(pageLinks).each(function(){
-							var that = this
-							that.removeClass('slideActive')
+							var that = this;
+							that.removeClass('slideActive');
 						})
-						link.addClass('slideActive')
+						link.addClass('slideActive');
 					}
 				})
 			})
@@ -79,28 +79,28 @@
 		
 		// Check current state and trigger elts binds depending on state
 		slider.checkState = function(current){
-			$([prev, next]).each(function(){ that = $(this); that.trigger('enable') })
+			$([prev, next]).each(function(){ that = $(this); that.trigger('enable') });
 			switch (current) {
 				case 1 : prev.trigger('disable'); break;
 				case slider.steps() : next.trigger('disable'); break;
 			}
-			if (!!obj.options.paginate) pageLinks[current-1].trigger('setActive')
+			if (!!obj.options.paginate) pageLinks[current-1].trigger('setActive');
 		}
 		
 		// Slide to the requested occurence
 		slider.slideTo = function(slideEq){
-			var slideOffset = dims.slideW*(slideEq-1)
+			var slideOffset = dims.slideW*(slideEq-1);
 			
 			slider.checkState(slideEq)
 			if (!animated) {
-				animated = true
+				animated = true;
 				slider.animate({
 					left: '-'+slideOffset,
 					duration: 500,
 					easing: easings.easeOut,
 					complete : function(){ 
-						current = slideEq
-						animated = false
+						current = slideEq;
+						animated = false;
 					}
 				})	
 			}
@@ -109,10 +109,10 @@
 		
 		// Binds on prev/next buttons
 		$([next, prev]).each(function(){
-			var btn = $(this)
+			var btn = $(this);
 			btn.bind({
-				disable : function(){ btn.addClass('buttonDisabled') },
-				enable : function(){ btn.removeClass('buttonDisabled') }
+				disable : function(){ btn.addClass('buttonDisabled'); },
+				enable : function(){ btn.removeClass('buttonDisabled'); }
 			})
 		})
 		
@@ -124,65 +124,68 @@
 					case 1 : direction = 'ltr'; break;
 					case slider.steps() : direction = 'rtl'; break;
 				}
-				slider.slideTo( ( direction === 'rtl' ) ? current-1 : current+1 ) 
-			}, obj.options.auto)
+				slider.slideTo( ( direction === 'rtl' ) ? current-1 : current+1 );
+			}, obj.options.auto);
 		}
 		// -- Pause autoplay
 		slider.pause = function(){
-			clearInterval(autoTimer)
-			autoTimer = null
+			clearInterval(autoTimer);
+			autoTimer = null;
 		}
 		// -- Stop autoplay
 		slider.stop  = function(){
-			slider.pause()
-			$(el).unbind('mouseenter, mouseleave')
+			slider.pause();
+			$(el).unbind('mouseenter, mouseleave');
 		}
 		// -- Init autoplay
 		slider.autoplay = function(){
 			$(el).bind({
-				mouseenter : function(){ slider.pause() },
-				mouseleave : function(){ if (!autoTimer) slider.play() }
+				mouseenter : function(){ slider.pause(); },
+				mouseleave : function(){ if (!autoTimer) slider.play(); }
 			})
-			slider.play()
+			slider.play();
 		}
 		
 		// Slider custom element initialization
 		slider.init = function(){
 
 			// -- Append layout & controls
-			wrapper.insertBefore(slider)
+			wrapper.insertBefore(slider);
 			
 			prev
 				.insertBefore(slider)
 				.unbind('click.prev')
-				.bind('click.prev', function(){ if ( current != 1 ) slider.slideTo(current-1) })
+				.bind('click.prev', function(){ if ( current != 1 ) slider.slideTo(current-1); });
 				
 			next
 				.insertAfter(slider)
 				.unbind('click.next')
-				.bind('click.next', function(){ if ( current != slider.steps() ) slider.slideTo(current+1) })
+				.bind('click.next', function(){ if ( current != slider.steps() ) slider.slideTo(current+1); });
 				
 			// If pagination 
-			if (!!obj.options.paginate) slider.paginate()
+			if (!!obj.options.paginate) slider.paginate();
 			
-			slider.checkState(current)
+			slider.checkState(current);
 			
-			slider.appendTo(wrapper).draw()
+			slider.appendTo(wrapper).draw();
 							
 			// -- Redraw on resize
-			$(window).resize(function(){ slider.draw() })
+			$(window).resize(function(){ slider.draw(); });
 			
 			// -- Autoslide
-			if (!!obj.options.auto) slider.autoplay()
+			if (!!obj.options.auto) slider.autoplay();
 		}
 		
-		slider.init()
+		slider.init();
 		
 		// Public methods
 		return {
-			play : function(){ slider.stop(); slider.autoplay() },
-			stop : function(){ slider.stop() },
-			slideTo : function(eq){ slider.slideTo(eq) }
+			play : function(){ 
+				slider.stop(); 
+				slider.autoplay(); 
+			},
+			stop : function(){ slider.stop(); },
+			slideTo : function(eq){ slider.slideTo(eq); }
 		}
 	}
-}(this)
+}(this);
